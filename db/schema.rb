31478165fc767fe0603fb132230bd200fe931493
Chrissy Atherholt <c.atherholt@gmail.com>
2015-04-30 21:04:30 -0400

    Added Devise, added movies scaffold, styled somethings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430013908) do
+ActiveRecord::Schema.define(version: 20150501010221) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "movie_length"
+    t.string   "director"
+    t.string   "rating"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -24,6 +34,10 @@ ActiveRecord::Schema.define(version: 20150430013908) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
