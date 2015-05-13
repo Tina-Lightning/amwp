1e444e1be354d4fe432b50316c7275a92409c981
Chrissy Atherholt <c.atherholt@gmail.com>
2015-05-12 21:18:00 -0400

    Added usernames to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513005038) do
+ActiveRecord::Schema.define(version: 20150513010216) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20150513005038) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
